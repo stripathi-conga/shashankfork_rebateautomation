1b345b005aac8d69a5eff4d4c8758198872dc028
APTTUS\stripathi <stripathi@apttus.com>
2019-09-20 18:29:11 +0530

    first commit

---
+++ src/main/java/com/apttus/aic/RebatesSFDC/App.java
@@ -0,0 +1,13 @@
+package com.apttus.aic.RebatesSFDC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
