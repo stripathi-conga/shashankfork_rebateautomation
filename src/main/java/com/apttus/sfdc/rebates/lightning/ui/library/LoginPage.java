00a1c2a9506416df46cad141c9c148bb1bc30bd4
Apttus_MituJain <mijain@apttus.com>
2019-12-09 15:19:34 +0530

    Modified .gitIgnore

---
+++ src/main/java/com/apttus/sfdc/rebates/lightning/ui/library/LoginPage.java
@@ -0,0 +1,50 @@
+package com.apttus.sfdc.rebates.lightning.ui.library;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.apttus.sfdc.rebates.lightning.common.GenericPage;
+
+public class LoginPage extends GenericPage {
+
+	@FindBy(id = "username")
+	@CacheLookup
+	public WebElement txtUserName;
+
+	@FindBy(id = "password")
+	@CacheLookup
+	public WebElement txtPassword;
+
+	@FindBy(id = "Login")
+	public WebElement btnLogin;
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+	public LoginPage waitForLoginPageLoad() throws Exception {
+
+		sfdcAcolyte.waitTillElementIsVisible(btnLogin).waitTillElementIsClickable(btnLogin);
+		return PageFactory.initElements(driver, LoginPage.class);
+
+	}
+
+	public HomePage loginToApp(String userName, String password) throws Exception {
+
+		sfdcAcolyte.waitTillElementIsClickable(txtUserName);
+		txtUserName.sendKeys(userName);
+		txtPassword.sendKeys(password);
+		btnLogin.click();
+		return PageFactory.initElements(driver, HomePage.class);
+	}
+
+	public LoginPage navigateToLoginPage(String LoginURL) throws Exception {
+		sfdcAcolyte.maximizeWindow();
+		sfdcAcolyte.navigateTo(LoginURL);
+		return PageFactory.initElements(driver, LoginPage.class);
+	}
+
+}
