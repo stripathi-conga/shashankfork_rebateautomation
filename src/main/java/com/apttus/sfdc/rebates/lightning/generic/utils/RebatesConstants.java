83119db0eaa84493659a962bfab2b2e84cfa1bb9
Apttus_MituJain <mijain@apttus.com>
2020-01-03 14:05:15 +0530

    Added missing file

---
+++ src/main/java/com/apttus/sfdc/rebates/lightning/generic/utils/RebatesConstants.java
@@ -0,0 +1,7 @@
+package com.apttus.sfdc.rebates.lightning.generic.utils;
+
+public class RebatesConstants {
+	
+	public static final String activate = "Active";
+	public static final String Deactivate = "Inactive";
+}
