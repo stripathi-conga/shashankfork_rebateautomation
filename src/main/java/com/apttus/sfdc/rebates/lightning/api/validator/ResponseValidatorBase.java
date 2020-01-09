bf253b9573a0f36e96aaf1c67c79713fbd8c6225
Apttus_MituJain <mijain@apttus.com>
2020-01-09 16:20:42 +0530

    Automation Code for create new program

--- src/main/java/com/apttus/sfdc/rebates/lightning/api/validator/ResponseValidatorBase.java
+++ src/main/java/com/apttus/sfdc/rebates/lightning/api/validator/ResponseValidatorBase.java
@@ -2,6 +2,8 @@ package com.apttus.sfdc.rebates.lightning.api.validator;
 
 import java.util.Map;
 import org.testng.asserts.SoftAssert;
+
+import com.apttus.sfdc.rebates.lightning.api.library.CIM;
 import com.apttus.sfdc.rebates.lightning.api.library.CIMAdmin;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -70,6 +72,35 @@ public class ResponseValidatorBase {
 		JsonObject records = resp.getAsJsonArray("records").get(0).getAsJsonObject();
 		softassert.assertEquals(records.get("Status__c").getAsString(),
 				status, "Validate status in linkTemplate");
+		softassert.assertAll();
+	}
+	
+	public void validateProgramDetails(Map<String, String> testData, Response response, CIM cim) {
+		softassert = new SoftAssert();
+		JsonObject resp = parser.parse(response.getBody().asString()).getAsJsonObject();
+		softassert.assertEquals(resp.get("totalSize").getAsInt(), 1,
+				"Validate response size, Response does not have single record");
+		softassert.assertAll();
+		JsonObject records = resp.getAsJsonArray("records").get(0).getAsJsonObject();
+		softassert.assertEquals(records.get("Apttus_Config2__EffectiveDate__c").getAsString(),
+				cim.programData.getApttus_Config2__EffectiveDate__c(), "Validate Program Start Date");
+		softassert.assertEquals(records.get("Apttus_Config2__ExpirationDate__c").getAsString(),
+				cim.programData.getApttus_Config2__ExpirationDate__c(), "Validate Program End Date");
+		softassert.assertEquals(records.get("BenefitLevel__c").getAsString(), testData.get("BenefitLevel__c"),
+				"Validate Program BenefitLevel");
+		softassert.assertEquals(records.get("MeasurementLevel__c").getAsString(), testData.get("MeasurementLevel__c"),
+				"Validate Program MeasurementLevel");
+		softassert.assertEquals(records.get("Currency__c").getAsString(), testData.get("Currency__c"),
+				"Validate Program Currency");
+		softassert.assertEquals(records.get("Id").getAsString(), cim.programData.getProgramId(), "Validate Program Id");
+		softassert.assertEquals(records.get("Name").getAsString(), cim.programData.getName(), "Validate Program Name");
+		softassert.assertEquals(records.get("Apttus_Config2__UseType__c").getAsString(),
+				testData.get("Apttus_Config2__UseType__c"), "Validate Program Type");
+		softassert.assertEquals(records.get("Apttus_Config2__SubUseType__c").getAsString(),
+				testData.get("Apttus_Config2__SubUseType__c"), "Validate Program SubType");
+		softassert.assertEquals(records.get("Program_Template_Id__c").getAsString(),
+				cim.programData.getProgram_Template_Id__c(), "Validate Program TemplateId");
+		softassert.assertEquals(records.get("Id").getAsString(), cim.programData.getProgramId(), "Validate Program Id");
 		softassert.assertAll();
 	}
 }
---
+++ src/test/java/com/apttus/sfdc/rebates/lightning/api/cim/TestIncentives.java
@@ -0,0 +1,55 @@
+package com.apttus.sfdc.rebates.lightning.api.cim;
+
+import java.util.Map;
+import java.util.Properties;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.apttus.helpers.Efficacies;
+import com.apttus.sfdc.rebates.lightning.api.library.CIM;
+import com.apttus.sfdc.rebates.lightning.api.validator.ResponseValidatorBase;
+import com.apttus.sfdc.rebates.lightning.generic.utils.SFDCHelper;
+import com.apttus.sfdc.rudiments.utils.SFDCRestUtils;
+import com.jayway.restassured.response.Response;
+
+public class TestIncentives {
+	private Properties configProperties;
+	protected String baseURL;
+	private Efficacies efficacies;
+	private SFDCRestUtils sfdcRestUtils;
+	protected SFDCHelper sfdcHelper;
+	private String instanceURL;
+	private ResponseValidatorBase responseValidator;
+	private CIM cim;
+	private Map<String, String> jsonData;
+	Response response;
+
+	@BeforeClass(alwaysRun = true)
+	@Parameters({ "runParallel", "environment", "browser", "hubURL" })
+	public void beforeClass(String runParallel, String environment, String browser, String hubURL) throws Exception {
+		efficacies = new Efficacies();
+		sfdcRestUtils = new SFDCRestUtils();
+		configProperties = efficacies.loadPropertyFile(environment);
+		baseURL = configProperties.getProperty("baseURL");
+		SFDCHelper.setMasterProperty(configProperties);
+		instanceURL = SFDCHelper.setAccessToken(sfdcRestUtils);
+		sfdcHelper = new SFDCHelper(instanceURL);
+		cim = new CIM(instanceURL, sfdcRestUtils);
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	public void beforeMethod() throws Exception {
+		responseValidator = new ResponseValidatorBase();
+	}
+
+	@Test(description = "TC345-Verify the creation of New Rebate Program", groups = { "Smoke", "API" })
+	public void createNewLoyaltyProgram() throws Exception {
+		jsonData = efficacies.readJsonElement("CIMTemplateData.json", "createNewProgram");
+		String programTemplateId = cim.getTemplateIdForProgram(jsonData);
+		jsonData.put("Program_Template_Id__c", programTemplateId);
+		cim.creatNewProgram(jsonData);
+		response = cim.getProgramDetails();
+		responseValidator.validateProgramDetails(jsonData, response, cim);
+	}
+}
