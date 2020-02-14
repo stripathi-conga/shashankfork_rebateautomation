5cebdbe8856a29836debbc9ea5bb1a3376deac06
ptingre <ptingre@apttus.com>
2020-02-14 14:13:33 +0530

    Addressed review comments. Moved reusable helper method out of the test class

---
+++ src/main/java/com/apttus/sfdc/rebates/lightning/generic/utils/IncentiveCreationHelper.java
@@ -0,0 +1,50 @@
+package com.apttus.sfdc.rebates.lightning.generic.utils;
+
+import java.util.Map;
+
+import com.apttus.helpers.Efficacies;
+import com.apttus.sfdc.rebates.lightning.api.library.CIM;
+import com.apttus.sfdc.rebates.lightning.api.validator.ResponseValidatorBase;
+import com.apttus.sfdc.rudiments.utils.SFDCRestUtils;
+import com.jayway.restassured.response.Response;
+
+public class IncentiveCreationHelper {
+	
+	private static String instanceURL;
+	private static SFDCRestUtils sfdcRestUtils;
+	private static Efficacies efficacies;
+	private static Response response;
+	private static ResponseValidatorBase responseValidator;
+	
+	public static Response CreateIncentiveAndFetchSchedules(String startDate, String endDate, String paymentFrequency)
+			throws Exception {
+		
+		sfdcRestUtils = new SFDCRestUtils();
+		instanceURL = SFDCHelper.setAccessToken(sfdcRestUtils);
+		efficacies = new Efficacies();		
+		responseValidator = new ResponseValidatorBase();
+		
+		CIM cim = new CIM(instanceURL, sfdcRestUtils);		
+		
+		// -------- Set-up JSON data for creating Incentive -----------------
+		Map<String, String> createIncentiveJson = efficacies.readJsonElement("CIMTemplateData.json", "activeTemplateIdForRebateDiscrete");
+		String incentiveTemplateIdBenefitProductDiscrete = cim.getTemplateIdForIncentives(createIncentiveJson);
+		createIncentiveJson = efficacies.readJsonElement("CIMTemplateData.json", "createNewIncentiveAgreementAccountBenefitProductDiscrete");
+		createIncentiveJson.put("ProgramTemplateId__c", incentiveTemplateIdBenefitProductDiscrete);
+		createIncentiveJson.put("Apttus_Config2__EffectiveDate__c", startDate);
+		createIncentiveJson.put("Apttus_Config2__ExpirationDate__c", endDate);
+		createIncentiveJson.put("PayoutFrequency__c", paymentFrequency);
+
+		// -------- Create and Validate Incentive -----------------
+		cim.createNewIncentive(createIncentiveJson);
+		response = cim.getIncentiveDetails();
+		responseValidator.validateIncentiveDetails(createIncentiveJson, response, cim);
+
+		// -------- Activate the incentive which will generate payout schedules -----------------
+		cim.activateIncentive();
+		response = cim.getIncentiveDetails();
+		responseValidator.validateIncentiveStatus(RebatesConstants.statusActivated, response, cim.getIncentiveData().incentiveId);
+		return cim.getPayoutSchedules();
+	}
+
+}
