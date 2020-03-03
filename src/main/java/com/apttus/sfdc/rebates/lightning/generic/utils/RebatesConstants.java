5bfc2544b4c865ddfbab7b9068623cebcb196523
APTTUS\stripathi <stripathi@apttus.com>
2020-03-03 18:43:40 +0530

    TC- 537 Verify error message when user tries to activate the program with unsaved changes

--- src/main/java/com/apttus/sfdc/rebates/lightning/generic/utils/RebatesConstants.java
+++ src/main/java/com/apttus/sfdc/rebates/lightning/generic/utils/RebatesConstants.java
@@ -42,6 +42,7 @@ public class RebatesConstants {
 	/* UI Constants      ------------------------------------------ */
 	public static final String messagequalificationformulavalidation="No data to display";
 	public static final String mandatoryMessageBenefitFormula="Benefit formula is required";
+	public static final String mandatoryMessageUnsavedChanges="Please save the unsaved changes to activate the Incentive";
 	public static final String messageFailToActivateWithoutParticipant="Failed to activate incentive - Incentive should have at least one participant configured.";
 	public static final String messageFailToActivateWithoutQnB="Failed to activate incentive - Incentive should have at least one benefit configured.";
 	public static final String viewPath = "view";
